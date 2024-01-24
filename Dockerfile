8f2d07c07147636a69f5441fc0636999d40b0336
Hemant Manohar Deshmukh <73582827+HemantDeshmukh026@users.noreply.github.com>
2024-01-24 01:50:42 -0500

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM quay.io/astronomer/astro-runtime:10.1.0
+ 
+RUN python -m venv soda_venv && source soda_venv/bin/activate && \
+    pip install --no-cache-dir soda-core-bigquery==3.0.45 &&\
+    pip install --no-cache-dir soda-core-scientific==3.0.45 && deactivate  
---
+++ dags/retail.py
@@ -0,0 +1,55 @@
+from airflow.decorators import dag, task
+from datetime import datetime
+
+from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
+from astro import sql as aql
+from astro.files import file
+from astro.sql.table import Table, Metadate
+from astro.constants import FIleType
+
+@dag(
+    start_date=datetime(2023,1,1),
+    schedule=None, 
+    catchup=False,
+    tags=['retail']
+)
+def retail():
+    upload_csv_to_gcs = LocalFilesystemToGCSOperator(
+        task_id='upload_csv_to_gcs',
+        src='include/dataset/online_retail.csv',
+        dst='raw/online_retail.csv',
+        bucket='marclamberti_online_retail',
+        gcp_conn_id='gcp',
+        mime_type='text/csv',
+    )
+
+    create_retail_dataset = BigQueryCreateEmptyDatasetOperator(
+        task_id='create_retail_dataset',
+        dataset_id='retail',
+        gcp_conn_id='gcp',
+    )
+
+    gcs_to_raw=aql.load_file(
+        task_id='gsc_to_raw',
+        input_file=File(
+            'gs://hemantdeshmukh_online_retail/raw/online_retail.csv',
+            conn_id='gcp',
+            filetype=FIleType.CSV,
+            ),
+            output_table=Table(
+                name='raw_invoices',
+                conn_id='gcp',
+                metadata=Metadata(schema='retail')
+            ),
+            use_native_support=False,
+    )
+
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    def check_load(scan_name='check_load', checks_subpath='sources'):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)  
+    check_load()
+
+retail() 
