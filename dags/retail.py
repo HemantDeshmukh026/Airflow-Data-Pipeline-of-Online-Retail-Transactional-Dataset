ba63548bd7f55527094b0f2770593323d59280e2
Hemant Manohar Deshmukh <73582827+HemantDeshmukh026@users.noreply.github.com>
2024-01-26 19:28:43 -0500

    Add files via upload

--- dags/retail.py
+++ dags/retail.py
@@ -6,7 +6,12 @@ from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmpt
 from astro import sql as aql
 from astro.files import file
 from astro.sql.table import Table, Metadate
+from airflow.models.baseoperator import chain
 from astro.constants import FIleType
+from include.dbt.cosmos_config import DBT_PROJECT_CONFIG, DBT_CONFIG
+from cosmos.airflow.task_group import DbtTaskGroup
+from cosmos.constants import LoadMode
+from cosmos.config import ProjectConfig, RenderConfig
 
 @dag(
     start_date=datetime(2023,1,1),
@@ -49,7 +54,47 @@ def retail():
     def check_load(scan_name='check_load', checks_subpath='sources'):
         from include.soda.check_function import check
 
-        return check(scan_name, checks_subpath)  
-    check_load()
+        return check(scan_name, checks_subpath)   
 
+    transform = DbtTaskGroup(
+        group_id='transform',
+        project_config=DBT_PROJECT_CONFIG,
+        profile_config=DBT_CONFIG,
+        render_config=RenderConfig(
+            load_method=LoadMode.DBT_LS,
+            select=['path:models/transform']
+        )
+    )
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    def check_transform(scan_name='check_transform', checks_subpath='transform'):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)  
+
+
+    report = DbtTaskGroup(
+        group_id='report',
+        project_config=DBT_PROJECT_CONFIG,
+        profile_config=DBT_CONFIG,
+        render_config=RenderConfig(
+            load_method=LoadMode.DBT_LS,
+            select=['path:models/report']
+        )
+    )
+
+    @task.external_python(python='/usr/local/airflow/soda_venv/bin/python')
+    def check_report(scan_name='check_report', checks_subpath='report'):
+        from include.soda.check_function import check
+
+        return check(scan_name, checks_subpath)
+
+    chain(
+        upload_csv_to_gcs,
+        create_retail_dataset,
+        gcs_to_raw,
+        check_load(),
+        check_transform(),
+        report,
+        check_report(),
+    )
 retail() 
---
+++ include/dbt/models/transform/dim_product.sql
@@ -0,0 +1,9 @@
+-- Create the dimension table
+SELECT DISTINCT
+    {{ dbt_utils.generate_surrogate_key(['StockCode', 'Description', 'UnitPrice']) }} as product_id,
+		StockCode AS stock_code,
+    Description AS description,
+    UnitPrice AS price
+FROM {{ source('retail', 'raw_invoices') }}
+WHERE StockCode IS NOT NULL
+AND UnitPrice > 0
